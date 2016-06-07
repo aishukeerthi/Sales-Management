8ec25a4008b1132c30f22cb3f34df78ece54bfd3
aishukeerthi <aishu96stella@gmail.com>
2016-06-07 19:36:13 +0530

    hello

--- test.c
+++ test.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main()
 {
-	printf("hello everyone welcome to Git");
-	printf("This is the edit made by Kiriti\n");
+	printf("hello everyone welcome to Git kanna");
+	printf("This is the edit made by Kanna\n");
+	printf("this is edited by aishu");
 	return 0;
 }
